13e08c8aa62be469483026042085cac0a9cbcb6f
tangliping <tangliping>
2024-04-11 16:27:47 +0800

    optimize

---
+++ dependent/libqrencode/build/CMakeFiles/qrenc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qrenc.dir/qrenc.c.o"
+  "CMakeFiles/qrenc.dir/qrenc.c.o.d"
+  "qrencode"
+  "qrencode.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/qrenc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ dependent/libqrencode/build/CMakeFiles/qrencode.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qrencode.dir/bitstream.c.o"
+  "CMakeFiles/qrencode.dir/bitstream.c.o.d"
+  "CMakeFiles/qrencode.dir/mask.c.o"
+  "CMakeFiles/qrencode.dir/mask.c.o.d"
+  "CMakeFiles/qrencode.dir/mmask.c.o"
+  "CMakeFiles/qrencode.dir/mmask.c.o.d"
+  "CMakeFiles/qrencode.dir/mqrspec.c.o"
+  "CMakeFiles/qrencode.dir/mqrspec.c.o.d"
+  "CMakeFiles/qrencode.dir/qrencode.c.o"
+  "CMakeFiles/qrencode.dir/qrencode.c.o.d"
+  "CMakeFiles/qrencode.dir/qrinput.c.o"
+  "CMakeFiles/qrencode.dir/qrinput.c.o.d"
+  "CMakeFiles/qrencode.dir/qrspec.c.o"
+  "CMakeFiles/qrencode.dir/qrspec.c.o.d"
+  "CMakeFiles/qrencode.dir/rsecc.c.o"
+  "CMakeFiles/qrencode.dir/rsecc.c.o.d"
+  "CMakeFiles/qrencode.dir/split.c.o"
+  "CMakeFiles/qrencode.dir/split.c.o.d"
+  "libqrencode.a"
+  "libqrencode.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/qrencode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
