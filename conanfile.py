de34e458d7271c6952bdc6fabcb1b6989a817a2f
Wisper <tph8666031@163.com>
2025-08-11 13:47:55 +0600

    optimize

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,6 @@
 from conan import ConanFile
+from conan.tools.files import copy
+import os
 
 class ComexConan(ConanFile):
    settings = "os", "compiler", "build_type", "arch"
@@ -6,24 +8,29 @@ class ComexConan(ConanFile):
    default_options = {"*:shared": False}
 
    def requirements(self):
-      self.requires("mosquitto/2.0.21")
-      self.requires("libcurl/8.12.1")
-      self.requires("libarchive/3.7.2")
-      self.requires("cairo/1.18.0")
-      self.requires("libuv/1.49.2")
-      self.requires("libzip/1.11.3")
-      self.requires("libiconv/1.17")
-      self.requires("libnfs/6.0.2")
-      self.requires("libqrencode/4.1.1")
-      self.requires("bzip2/1.0.8")
-      self.requires("openssl/3.5.2")
-      self.requires("podofo/1.0.1")
-      self.requires("libsmb2/6.2")
-      self.requires("libmagic/5.46-f1")
-      self.requires("libjpeg/9f")
-      if self.settings.os != "Windows":
-        self.requires("libtar/1.2.20")
+       self.requires("mosquitto/2.0.21")
+       self.requires("libcurl/8.12.1")
+       self.requires("libarchive/3.7.2")
+       self.requires("cairo/1.18.0")
+       self.requires("libuv/1.49.2")
+       self.requires("libzip/1.11.3")
+       self.requires("libiconv/1.17")
+       self.requires("libnfs/6.0.2")
+       self.requires("libqrencode/4.1.1")
+       self.requires("bzip2/1.0.8")
+       self.requires("openssl/3.5.2")
+       self.requires("podofo/1.0.1")
+       self.requires("libsmb2/6.2")
+       self.requires("libmagic/5.46-f1")
+       self.requires("libjpeg/9f")
+       if self.settings.os != "Windows":
+          self.requires("libtar/1.2.20")
         
-   def imports(self):
-      self.copy("*", dst="out/bin", src="bin")
-      self.copy("*.a", dst="out/lib", src="lib")
+   def generate(self):
+       for dep in self.dependencies.values():
+           for libdir in dep.cpp_info.libdirs:
+               copy(self, "*.a", libdir, os.path.join(self.build_folder, "..","out","lib"))
+               copy(self, "*.dylib", libdir, os.path.join(self.build_folder, "..","out","lib"))
+               copy(self, "*.dll", libdir, os.path.join(self.build_folder, "..","out","lib"))
+           for includedir in dep.cpp_info.includedirs:
+               copy(self, "*.h", includedir, os.path.join(self.build_folder, "..","out","include"))
